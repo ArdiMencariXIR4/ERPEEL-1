805ae357846d9a45cd3d9a8228c26ed4173cd620
ardian-xir4 <h1m4j0@gmail.com>
2026-04-02 14:38:47 +0700

    fix default port to 8080

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,7 @@ async function bootstrap() {
 
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  await app.listen(process.env.PORT ?? 8080, '0.0.0.0');
 }
 
 bootstrap();
